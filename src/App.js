d0501fb8bae03c96a7e8f9615cbf6e52fd7d14c0
pollman <caleb.pollman@zeeram.com>
2018-02-26 15:04:57 -0700

    prepared init branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import Calculator from './components/Calculator/Calculator';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        Hello World!
       </div>
     );
   }
